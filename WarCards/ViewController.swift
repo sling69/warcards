a996ac2c57c6a7abebafb0f83e169793fa814d5c
Sye Declerck <sye@syes-air.home>
2016-10-28 21:13:08 +1100

    Initial Commit

---
+++ WarCards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WarCards
+//
+//  Created by Sye Declerck on 28/10/16.
+//  Copyright © 2016 Daquiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
