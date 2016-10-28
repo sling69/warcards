cd06f72189ecd13922ed74de1585f5d06207c3c2
Sye Declerck <sye@syes-air.home>
2016-10-29 10:13:22 +1100

    Connected to ViewController

--- WarCards/ViewController.swift
+++ WarCards/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    
+    @IBOutlet weak var playRoundButton: UIButton!
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.playRoundButton.setTitle("Play", for: UIControlState.normal)
     }
 
     override func didReceiveMemoryWarning() {
